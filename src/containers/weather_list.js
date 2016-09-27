8b3d94be99260b75225a3ad41343daa1c8d4a94f
Mali Michael <malimccalla@gmail.com>
2016-09-27 22:07:38 +0100

    Weather list component

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 
 class WeatherList extends Component {
+
+  renderWeather(cityData) {
+    return (
+      <tr>
+        <td>{cityData.city.name}</td>
+      </tr>
+    );
+  }
+
+
   render() {
     return (
       <table className="table table-hover">
@@ -14,17 +24,15 @@ class WeatherList extends Component {
           </tr>
         </thead>
         <tbody>
-
+          {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     );
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    weather: state.weather
-  }
+function mapStateToProps({weather}) { // const weather = state.weather
+  return {weather}; // { weather } === { weather: weather} ES6
 }
 
-export default connect(mapStateToProps)(WeatherList)
+export default connect(mapStateToProps)(WeatherList);
